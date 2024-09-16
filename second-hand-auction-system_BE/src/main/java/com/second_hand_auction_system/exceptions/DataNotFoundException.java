329daf78ff686c05f6eaabebca3caf1d0e56d3ba
Nguyễn Đức Anh <133002625+NguyenDucAnh1908@users.noreply.github.com>
2024-09-16 09:15:23 +0700

    authored-by:DucAnh : Add Exception and i18n to transfer language

---
+++ second-hand-auction-system_BE/src/main/java/com/second_hand_auction_system/exceptions/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package com.second_hand_auction_system.exceptions;
+
+public class DataNotFoundException extends Exception{
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ second-hand-auction-system_BE/src/main/java/com/second_hand_auction_system/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.second_hand_auction_system.exceptions;
+
+import com.second_hand_auction_system.responses.ResponseObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler
+{
+    //try-catch exception global
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ResponseObject> handleGeneralException(Exception exception){
+        return ResponseEntity.internalServerError().body(
+                ResponseObject.builder()
+                        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .message(exception.getMessage())
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(DataNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ResponseObject> handleResourceNotFoundException(Exception exception){
+        return ResponseEntity.internalServerError().body(
+                ResponseObject.builder()
+                        .status(HttpStatus.NOT_FOUND)
+                        .message(exception.getMessage())
+                        .build()
+        );
+    }
+}
