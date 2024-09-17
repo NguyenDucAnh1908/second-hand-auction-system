5109e05cd27e3ae8eb41d877afd7f7b5fadf5f97
chanhtong123 <chanhtong11@gmail.com>
2024-09-17 11:53:48 +0700

    done

--- second-hand-auction-system_FE/src/main.jsx
+++ second-hand-auction-system_FE/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles/tailwind.css";
+import "./styles/index.css";
+import "./styles/font.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
+
+
+
