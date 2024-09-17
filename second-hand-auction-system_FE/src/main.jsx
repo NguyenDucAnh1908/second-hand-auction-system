82e2422506562195c0976f29e8a613ddbd854f0b
Nguyễn Đức Anh <133002625+NguyenDucAnh1908@users.noreply.github.com>
2024-09-17 14:32:36 +0700

    DucAnh
    Config redux

--- second-hand-auction-system_FE/src/main.jsx
+++ second-hand-auction-system_FE/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
-import React from "react";
+//import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/tailwind.css";
 import "./styles/index.css";
 import "./styles/font.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store/configureStoreRedux.js";
 
 // const container = document.getElementById("root");
 // const root = createRoot(container);
@@ -14,12 +16,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <Provider store={store}> */}
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-    {/* </Provider> */}
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
