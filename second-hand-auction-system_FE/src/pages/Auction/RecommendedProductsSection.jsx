96c55861170c0c97c995c58dee28e1baf255a3dd
chanhtong123 <chanhtong11@gmail.com>
2024-09-17 09:56:44 +0700

    Auction

--- second-hand-auction-system_FE/src/pages/Auction/RecommendedProductsSection.jsx
+++ second-hand-auction-system_FE/src/pages/Auction/RecommendedProductsSection.jsx
@@ -2,24 +2,24 @@ import { Button, Img, Slider, Heading } from "../../components";
 import ProductDetails4 from "../../components/ProductDetails4";
 import React from "react";
 
-export default function RecentProductsSection() {
+export default function RecommendedProductsSection() {
   const [sliderState, setSliderState] = React.useState(0);
   const sliderRef = React.useRef(null);
 
   return (
     <>
-      {/* recent products section */}
-      <div className="mt-[180px] flex flex-col items-center self-stretch">
-        <div className="container-xs flex flex-col items-start gap-[30px] px-1 md:px-5">
+      {/* recommended products section */}
+      <div className="mt-[198px] flex flex-col items-center self-stretch">
+        <div className="container-xs flex flex-col items-center gap-[72px] md:gap-[54px] md:px-5 sm:gap-9">
           <Heading
             size="text7xl"
             as="h2"
             className="text-[28px] font-medium text-blue_gray-900_01 md:text-[26px] sm:text-[24px]"
           >
-            Sản phẩm gần đây
+            Có thể bạn sẽ thích
           </Heading>
-          <div className="relative ml-2 mr-1 h-[454px] content-center self-stretch md:mx-0 md:h-auto">
-            <div className="mx-auto flex w-full gap-8 md:flex-col">
+          <div className="relative h-[452px] content-center self-stretch md:h-auto">
+            <div className="mx-auto flex w-full gap-[30px] md:flex-col">
               <Slider
                 autoPlay
                 autoPlayInterval={2000}
@@ -32,18 +32,14 @@ export default function RecentProductsSection() {
                 ref={sliderRef}
                 items={[...Array(12)].map(() => (
                   <React.Fragment key={Math.random()}>
-                    <div className="px-4">
-                      <ProductDetails4
-                        productDescription="Áo hoodie Nike thấm hút mồ hôi cho ngày đông lạnh "
-                        originalPrice="128.000đ"
-                        className="border border-solid border-gray-200 bg-bg-white"
-                      />
+                    <div className="px-[15px]">
+                      <ProductDetails4 />
                     </div>
                   </React.Fragment>
                 ))}
               />
             </div>
-            <div className="absolute left-0 right-0 top-[37%] m-auto flex flex-1 justify-between gap-5">
+            <div className="absolute left-0 right-0 top-[41%] m-auto flex flex-1 justify-between gap-5">
               <Button
                 onClick={() => {
                   sliderRef?.current?.slidePrev();
@@ -56,7 +52,7 @@ export default function RecentProductsSection() {
                 onClick={() => {
                   sliderRef?.current?.slideNext();
                 }}
-                className="w-[40px] rounded-[50%] border border-solid border-blue_gray-900 px-3"
+                className="mr-1.5 w-[44px] rounded-[22px] border border-solid border-blue_gray-900 px-3.5"
               >
                 <Img src="images/img_arrow_right_blue_gray_900_1.svg" />
               </Button>
